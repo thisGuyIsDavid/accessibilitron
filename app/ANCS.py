a412a143d49f6ce29af0c88bed3b321b5a379aa7
David <dhaverberg@medicalalley.org>
2025-10-18 20:43:16 -0500

    adding more ancs work

--- app/ANCS.py
+++ app/ANCS.py
@@ -26,7 +26,7 @@ class ANCS:
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
-            timeout=1
+            timeout=3
         )
 
     def setup_active_notifications(self):
