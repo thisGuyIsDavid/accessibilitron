df39b83fccee4f047b08f1a24d1e4e2bf07fc69b
David <dhaverberg@medicalalley.org>
2025-10-19 08:07:16 -0500

    adding more ancs work

--- ReANCS.py
+++ ReANCS.py
@@ -6,11 +6,6 @@ import time
 with serial.Serial(
         port='/dev/serial0', baudrate=9600, parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0) as ser:
-    ser.write("WAKEUP".encode())
-    for i in range(2):
-        ser.readline()
-        time.sleep(0.5)
-    print('sending')
     ser.write("AT+ADDR?".encode())
     while True:
         x = ser.readline()
