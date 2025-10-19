1f36721e7692e8c79256b91f5b790c3b0e31b43b
David <dhaverberg@medicalalley.org>
2025-10-19 07:46:03 -0500

    adding more ancs work

--- ReANCS.py
+++ ReANCS.py
@@ -3,5 +3,8 @@ import serial
 with serial.Serial(
         port='/dev/serial0', baudrate=9600, parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1) as ser:
+    serial.write("WAKEUP".encode())
+    print(serial.readline())
+
     print(f"Serial port {ser.name} opened successfully.")
 
